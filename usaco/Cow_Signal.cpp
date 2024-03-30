d70163f85ddfb140307ae4dee12b724228c6fe45
Omar Aziz <omarazizaustin@gmail.com>
2024-03-30 18:31:15 -0500

    Working cow signal solution

--- usaco/Cow_Signal.cpp
+++ usaco/Cow_Signal.cpp
@@ -8,11 +8,40 @@
 */
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-  cout << "Hello World" << endl;
-  int k = 64;
-  return 0;
+  
+  freopen("cowsignal.in", "r", stdin);
+  freopen("cowsignal.out", "w", stdout);
+  int M, N, K; //M is y_dim, N is x_dim, K is dilation_val
+  cin >> M >> N >> K;
+  cout << endl;
+  string lines = "";
+  for (int i = 0; i < M; i++){
+    string new_line = "";
+    for (int j = 0; j < N; j++){
+      char z;
+      cin >> z;
+        for (int l = 0; l < K; l++){
+          new_line += z;
+        }
+  }
+    if (i == M-1){
+      for (int l = 0; l < K - 1; l++){
+        lines += new_line;
+        lines += "\n";
+      }
+      lines += new_line;
+    }
+    else{
+      for (int l = 0; l < K; l++){
+        lines += new_line;
+        lines += "\n";
+      }
+    }
+}
+  cout << lines;
 }
 
