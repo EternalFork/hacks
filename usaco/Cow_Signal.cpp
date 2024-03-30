8818512bcb4c44c0c587976842d4022f9d69858a
Omar Aziz <omarazizaustin@gmail.com>
2024-03-30 18:22:55 -0500

    First working cpp

---
+++ usaco/Cow_Signal.cpp
@@ -0,0 +1,18 @@
+/* Input Format
+*  5 4 2
+*  XXX.
+*  X..X
+*  XXX.
+*  X..X
+*  XXX.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "Hello World" << endl;
+  int k = 64;
+  return 0;
+}
+
